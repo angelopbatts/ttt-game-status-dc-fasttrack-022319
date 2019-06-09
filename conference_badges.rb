428d8fcd97444fd0f0ea0a34d21cf2470e4d12c4
Angelo Batts <angelopbatts@gmail.com>
2019-06-09 19:29:27 +0000

    Automatically backed up by Learn

---
+++ conference_badges.rb
@@ -0,0 +1,12 @@
+badge_maker("Edsger")
+=> "Hello, my name is Edsger."
+batch_badge_creator
+
+def "Edsger","Ada","Charles","Alan","Grace","Linus","Matz"
+puts 
+end
+
+assign_rooms ["1","2","3","4","5","6","7"]
+puts
+end
+each_with_index
